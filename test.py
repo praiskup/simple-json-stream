329882053b59e1af0e45efd2a7f3a0a67f2b98e4
Pavel Raiskup <praiskup@redhat.com>
2021-10-15 15:13:00 +0200

    Some PyLint cleanup

--- test.py
+++ test.py
@@ -1,28 +1,42 @@
 #! /usr/bin/python3
 
-import sys
-from yajl import *
-import sys
+"""
+Read JSON as stream.
+"""
 
+import sys
 import logging
+import pprint
+
+from yajl import YajlContentHandler, YajlParser
+
+
 #logging.basicConfig(format="%(message)s", level=logging.DEBUG)
 log = logging.getLogger()
 
 
-def to_utf8(wrapped_method):
-    def arg_utf8_converter(a, b, c):
-        return wrapped_method(a, b, c.decode("utf8"))
-
+def params_to_utf8(wrapped_method):
+    """
+    The arguments given by Yajl are byte strings, so convert them to UTF
+    """
+    def arg_utf8_converter(arg1, arg2, arg3):
+        return wrapped_method(arg1, arg2, arg3.decode("utf8"))
     return arg_utf8_converter
 
 
 class StructPointer:
+    """
+    Node in the stack of JSON nodes
+    """
     def __init__(self, initial_value):
         self.document = initial_value
         self.type = type(initial_value)
         self.pointer = None
 
     def set_value(self, value):
+        """
+        Set the value of the currently processed field (given by pointer)
+        """
         if isinstance(self.document, dict):
             self.document[self.pointer] = value
         else:
@@ -33,6 +47,7 @@ class StructPointer:
                 self.pointer += 1
 
     def set_pointer(self, pointer):
+        """ Setup the currently filled-up key """
         self.pointer = pointer
 
     def __str__(self):
@@ -40,13 +55,14 @@ class StructPointer:
             return "<>"
         if isinstance(self.document, dict):
             return str(self.pointer)
-        else:
-            return str(self.pointer)
+        return str(self.pointer)
 
 
-# Sample callbacks, which output some debug info
-# these are examples to show off the yajl parser
 class ContentHandler(YajlContentHandler):
+    """
+    Override the generic content handler
+    """
+
     def __init__(self, to_stream):
         self.to_stream = to_stream
         self.full_output = None
@@ -80,24 +96,26 @@ class ContentHandler(YajlContentHandler):
         return self._current_path_inner(True)
 
     def yajl_null(self, ctx):
-        log.info("{} setting null".format(self._current_path))
+        log.info("%s setting null", self._current_path)
         self._set_value(None)
 
     def yajl_boolean(self, ctx, boolVal):
-       log.info("{} setting bool: {}".format(self._current_path,  boolVal))
-       self._set_value(boolVal)
+        log.info("%s setting bool: %s", self._current_path,  boolVal)
+        self._set_value(boolVal)
 
-    def yajl_integer(self, ctx, integerVal):
-        log.info("{} setting int: {}".format(self._current_path,  integerVal))
-        self._set_value(integerVal)
+    def yajl_integer(self, _, integer):
+        """ store int """
+        log.info("%s setting int: %s", self._current_path,  integer)
+        self._set_value(integer)
 
-    def yajl_double(self, ctx, doubleVal):
-        log.info("{} setting float: {}".format(self._current_path,  doubleVal))
-        self._set_value(doubleVal)
+    def yajl_double(self, _, double):
+        """ store float """
+        log.info("%s setting float: %s", self._current_path,  double)
+        self._set_value(double)
 
-    @to_utf8
+    @params_to_utf8
     def yajl_string(self, ctx, stringVal):
-        log.info("{} setting string: {}".format(self._current_path, stringVal))
+        log.info("%s setting string: %s", self._current_path, stringVal)
         self._set_value(stringVal)
 
     def yajl_start_map(self, ctx):
@@ -107,7 +125,7 @@ class ContentHandler(YajlContentHandler):
         log.info("%s <= setting map", self._current_path_minus_one)
         self.document_pointer = StructPointer(new_map)
 
-    @to_utf8
+    @params_to_utf8
     def yajl_map_key(self, ctx, stringVal):
         self.document_pointer.set_pointer(stringVal)
         log.info("%s (going to key '%s')", self._current_path_minus_one, stringVal)
@@ -121,7 +139,7 @@ class ContentHandler(YajlContentHandler):
         new_array = []
         self._set_value(new_array)
         self.document_pointer_stack.append(self.document_pointer)
-        new_pointer = self.document_pointer = StructPointer(new_array)
+        self.document_pointer = StructPointer(new_array)
 
     def yajl_end_array(self, ctx):
         self.document_pointer = self.document_pointer_stack.pop()
@@ -134,3 +152,5 @@ parser = YajlParser(handler)
 # Parse JSON from stdin
 with open(sys.argv[1], "rb") as f:
     parser.parse(f=f)
+
+pprint.pprint(handler.document)
